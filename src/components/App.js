82e5ff4329f4969b481188915808c29964926c9e
bradleymbuvi <bradwent@gmail.com>
2024-02-08 00:42:47 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
@@ -6,17 +6,25 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  
+  
+  const [button,setMode] = useState(false)
+  const appClass = button ? "App dark" : "App light"
+  const handleMode=()=>{
+    setMode(!button)
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
         <button>Dark Mode</button>
+        <button onClick={handleMode}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
   );
 }
+
 
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState} from "react";
 
 function Item({ name, category }) {
+  const [add,setAdd] = useState(false)
+  const addClass = add?"in-cart":""
+  const handleMode = ()=>{
+    setAdd(!add)
+  }
   return (
-    <li className="">
+    <li className={addClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
       <button className="add">Add to Cart</button>
+      <button onClick={handleMode}className="add">Add to Cart</button>
     </li>
   );
 }
